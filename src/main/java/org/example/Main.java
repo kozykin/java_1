969097b778403f29589c30549c234046aebf7542
kozykin.k <kozykinka@alfastrah.ru>
2022-11-25 23:13:34 +0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte varByte = 100;
+        short varShort = 200;
+        int varInt = 500;
+        long varLong = 400L;
+        double varDouble = 0.88;
+        float varFloat = 34.56f;
+
+
+        System.out.println(varByte + varShort);
+        System.out.println(varInt - varLong);
+        System.out.println(varLong * varDouble);
+        System.out.println(varFloat / varDouble);
+        System.out.println(varInt % varLong);
+
+        int a = Integer.MAX_VALUE;
+        System.out.println(a);
+        System.out.println(a + 4);
+
+
     }
 }
